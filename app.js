c961ac3a6c20ab27892b3e6b934c5896409ffe5c
Alex Piter <alexsoares2517@gmail.com>
2021-11-04 21:12:07 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// JSON & View Engine
+app.use(express.json());
+app.set("view engine", "ejs");
+
+
+// Rota Index
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+
+// Inicialização do servidor
+app.listen(port, () => {
+  console.log(`Server running at port http://localhost:${port}`);
+});
